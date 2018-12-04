9787fcd9a074c6ca60cc661a9158259ae1fff60a
Radek Em <guit4rfre4k@github.io>
2018-12-04 22:35:13 +0100

    Initial commit
    Signed-off-by: Radek Em <guit4rfre4k@github.io>

---
+++ src/main/java/io/github/guit4rfre4k/wtfs/ConditionalOperatorWtfDemo.java
@@ -0,0 +1,14 @@
+package io.github.guit4rfre4k.wtfs;
+
+/**
+ * This example shows an interesting behaviour with ? : operator and
+ * auto-boxing/unboxing.
+ */
+public class ConditionalOperatorWtfDemo {
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
