cbc4eeae20f46dbaf5c5331fa1023c819bcd7e59
Radek Em <guit4rfre4k@github.io>
2018-12-04 22:50:27 +0100

    Add an example with auto unboxing in conditional operator.
    Signed-off-by: Radek Em <guit4rfre4k@github.io>

--- src/main/java/io/github/guit4rfre4k/wtfs/ConditionalOperatorWtfDemo.java
+++ src/main/java/io/github/guit4rfre4k/wtfs/ConditionalOperatorWtfDemo.java
@@ -1,13 +1,27 @@
 package io.github.guit4rfre4k.wtfs;
 
+import java.util.logging.Logger;
+
 /**
- * This example shows an interesting behaviour with ? : operator and
- * auto-boxing/unboxing.
+ * This class demonstrates the problem with implicit unboxing in the conditional ? : operator which
+ * leads to quite unexpected NullPointerException.
+ * As a background see: https://docs.oracle.com/javase/specs/jls/se7/html/jls-15.html#jls-15.25
  */
 public class ConditionalOperatorWtfDemo {
 
+    private static final Logger LOG = Logger.getLogger(ConditionalOperatorWtfDemo.class.getName());
+
     public static void main(String[] args) {
-        
+
+        Double doubleValue1 = null;
+        boolean booleanValue = true;
+
+        // According to JLS, the resulting value of the conditional expression is double (see: JLS -> 15.25.")
+        // As a consequence, while evaluating a final value of the conditional expression,
+        // Java tries to unbox the second operand what (because it's null) leads to a NPE.
+        //
+        Double resultValue = booleanValue ? doubleValue1 : 1.0;  // boolean ? Double : double  --> Double is tired to be unboxed
+        LOG.info("Result value is: " + resultValue);
     }
 
 
