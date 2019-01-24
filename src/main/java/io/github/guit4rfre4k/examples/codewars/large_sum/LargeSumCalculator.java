0f74688580ed1866d3380485d9ed368524801f9e
guit4rfre4k <guit4rfre4k@test.com>
2019-01-24 23:16:51 +0100

    Codewars: Add implemetaion for large sums
    Signed-off-by: guit4rfre4k <guit4rfre4k@test.com>

---
+++ src/main/java/io/github/guit4rfre4k/examples/codewars/large_sum/LargeSumCalculator.java
@@ -0,0 +1,13 @@
+package io.github.guit4rfre4k.examples.codewars.large_sum;
+
+/**
+ * The first ten digits of the sum of the following one-hundred 50-digit numbers are 5537376230
+ * (sum is 5537376230390876637302048746832985971773659831892672) (see Example Test Case).
+ * Write a method that returns the first ten digits of the sum of one-hundred random 50-digit
+ * numbers you get as String array.
+ */
+public interface LargeSumCalculator {
+
+    public String firstTenDigitsOfSum(String[] numbers);
+
+}
