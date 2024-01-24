3d7b90f745dfe76ee0ee514ee883b2b402623b15
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2024-01-24 21:52:01 +0100

    [LeetCode][addTwoNumbers] First accepted version

--- src/main/java/com/gitlab/emradbuba/learning/java_examples/leetcode/addtwonumbers/AddTwoNumbers.java
+++ src/main/java/com/gitlab/emradbuba/learning/java_examples/leetcode/addtwonumbers/AddTwoNumbers.java
@@ -1,9 +1,41 @@
 package com.gitlab.emradbuba.learning.java_examples.leetcode.addtwonumbers;
 
+import java.math.BigInteger;
+
 public class AddTwoNumbers {
 
     // Actual solution method:
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        int inMemo = 0;
+        ListNode currentL1 = l1;
+        ListNode currentL2 = l2;
+        ListNode resultTopNode = new ListNode();
+        ListNode recentlyAddedNode = resultTopNode;
+
+        do {
+            int sum = currentL1.val + currentL2.val + inMemo;
+            inMemo = sum >= 10 ? 1 : 0;
+            ListNode newNode = new ListNode(sum % 10);
+            recentlyAddedNode.next = newNode;
+            recentlyAddedNode = newNode;
+            currentL1 = currentL1.next;
+            currentL2 = currentL2.next;
+        } while (currentL1 != null && currentL2 != null);
+
+        ListNode notFinishedListNode = currentL1 == null ? currentL2 : currentL1;
+        while (notFinishedListNode != null) {
+            int sum = notFinishedListNode.val + inMemo;
+            inMemo = sum >= 10 ? 1 : 0;
+            ListNode newNode = new ListNode(sum % 10);
+            recentlyAddedNode.next = newNode;
+            recentlyAddedNode = newNode;
+            notFinishedListNode = notFinishedListNode.next;
+        }
+        if (inMemo == 1) {
+            ListNode newNode = new ListNode(1);
+            recentlyAddedNode.next = newNode;
+        }
+
+        return resultTopNode.next;
     }
 }
