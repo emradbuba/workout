5bcd0e97e472d5c0679d58a3594f3b6c324d531c
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2024-01-26 08:44:08 +0100

    [LeetCode][addTwoNumbers] Refactored version with one loop and ? operator

--- src/main/java/com/gitlab/emradbuba/learning/java_examples/leetcode/addtwonumbers/AddTwoNumbersFirstApproachRefactored.java
+++ src/main/java/com/gitlab/emradbuba/learning/java_examples/leetcode/addtwonumbers/AddTwoNumbersFirstApproachRefactored.java
@@ -3,25 +3,17 @@ package com.gitlab.emradbuba.learning.java_examples.leetcode.addtwonumbers;
 public class AddTwoNumbersFirstApproachRefactored implements AddTwoNumbersSolution {
 
     // Actual solution method:
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers(ListNode firstList, ListNode secondList) {
         int inMemo = 0;
         ListNode resultListTop = null;
         ListNode recentlyAddedNode = resultListTop;
 
-        while (l1 != null || l2 != null || inMemo > 0) {
-            int sum = 0;
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            if (inMemo > 0) {
-                sum += inMemo;
-                inMemo = 0;
-            }
+        while (firstList != null || secondList != null || inMemo > 0) {
+            int sum = (firstList != null ? firstList.val : 0)
+                    + (secondList != null ? secondList.val : 0)
+                    + inMemo;
+            if(firstList != null) firstList = firstList.next;
+            if(secondList != null) secondList = secondList.next;
             inMemo = sum / 10;
             ListNode newListNode = new ListNode(sum % 10);
             if(resultListTop == null) {
