8aebe834b5b79786289bca908de057b7ab55c005
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2024-01-29 19:32:02 +0100

    [LeetCode][LinkedListReverse] First version of recursive solution

--- src/main/java/com/gitlab/emradbuba/learning/java_examples/leetcode/linkedlistreverse/LinkedListSoReverseRecursiveSolution.java
+++ src/main/java/com/gitlab/emradbuba/learning/java_examples/leetcode/linkedlistreverse/LinkedListSoReverseRecursiveSolution.java
@@ -3,7 +3,7 @@ package com.gitlab.emradbuba.learning.java_examples.leetcode.linkedlistreverse;
 public class LinkedListSoReverseRecursiveSolution implements LinkedListReverseSolution {
     @Override
     public ListNode reverseList(ListNode head) {
-        if(head == null) return null;
+        if (head == null) return null;
         if (head.next == null) {
             return new ListNode(head.val);
         }
