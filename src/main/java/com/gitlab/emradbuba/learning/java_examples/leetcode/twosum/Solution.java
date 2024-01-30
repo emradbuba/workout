0c8231f26064a11a20920ae03c00903e2cc96ade
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2024-01-30 08:30:29 +0100

    [LeetCode][AddTwo] Initial native starting point for improvements (passed)

---
+++ src/main/java/com/gitlab/emradbuba/learning/java_examples/leetcode/twosum/Solution.java
@@ -0,0 +1,15 @@
+package com.gitlab.emradbuba.learning.java_examples.leetcode.twosum;
+
+public class Solution {
+    // TODO: create tests and improve efficiency? Extra hash? preSorting?
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
