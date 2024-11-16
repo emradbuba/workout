e6c458597b48f4616741629f6ab6f641ea3150bc
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2024-11-16 18:12:00 +0100

    TrainingSession BinarySerach

---
+++ src/main/java/com/emradbuba/learning/workout/leetcode/binarysearch_704/BinarySearchLoopRecursiveSolution.java
@@ -0,0 +1,9 @@
+package com.emradbuba.learning.workout.leetcode.binarysearch_704;
+
+public class BinarySearchLoopRecursiveSolution implements BinarySearchSolution {
+
+    @Override
+    public int search(int[] nums, int target) {
+        return -50;
+    }
+}
