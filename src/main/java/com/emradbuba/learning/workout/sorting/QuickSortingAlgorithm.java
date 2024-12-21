c509a24fd3ee0492cf061a099e25a4642b1d6b81
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2024-12-21 15:28:12 +0100

    Test comit

--- src/main/java/com/emradbuba/learning/workout/sorting/QuickSortingAlgorithm.java
+++ src/main/java/com/emradbuba/learning/workout/sorting/QuickSortingAlgorithm.java
@@ -3,7 +3,7 @@ package com.emradbuba.learning.workout.sorting;
 public class QuickSortingAlgorithm implements SortingAlgorithm {
     @Override
     public int[] sortArray(int[] array) {
-        // TODO: Implememt
+        // TODO: To implement
         return null;
     }
 }
